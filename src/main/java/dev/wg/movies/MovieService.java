e81ea3d547b7ba62cdfba5a5fe5d30248b8ba60e
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-19 00:56:35 -0400

    add movieService and confirm getAllMovies request in the movie Controller api/v1/movies endpoint

--- src/main/java/dev/wg/movies/MovieService.java
+++ src/main/java/dev/wg/movies/MovieService.java
@@ -1,2 +1,17 @@
-package dev.wg.movies;public class MovieService {
+package dev.wg.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired //let spring instantiate below class for us
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
 }
