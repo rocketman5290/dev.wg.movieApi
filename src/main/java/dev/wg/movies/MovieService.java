9154b751a10c74981226c52789b51f5c1a89639b
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-19 18:20:34 -0400

    Additional requests

--- src/main/java/dev/wg/movies/MovieService.java
+++ src/main/java/dev/wg/movies/MovieService.java
@@ -1,10 +1,11 @@
 package dev.wg.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,6 +13,10 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 
 }
--- src/main/java/dev/wg/movies/MoviesApplication.java
+++ src/main/java/dev/wg/movies/MoviesApplication.java
@@ -2,12 +2,13 @@ package dev.wg.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(MoviesApplication.class, args);
 	}
 
 }
