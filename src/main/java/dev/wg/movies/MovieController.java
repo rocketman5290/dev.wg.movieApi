9038d2637e86f749abb09c0bce23a3e1cbd7f989
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-19 00:55:21 -0400

    Add movie controller

--- src/main/java/dev/wg/movies/MovieController.java
+++ src/main/java/dev/wg/movies/MovieController.java
@@ -1,2 +1,22 @@
-package dev.wg.movies;public class MovieController {
+package dev.wg.movies;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired // Let spring instantiate this service instance for us using Autowired annotations
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
 }
